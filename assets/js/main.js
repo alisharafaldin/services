a22b03afbbeae2741d5a93d4bac8f726e4b09822
alisharafaldinclear <alisharafaldin@gmail.com>
2021-10-05 16:30:11 +0300

    Change_4

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,12 +12,13 @@ $(function () {
     loop: true,
     margin: 10,
     nav: true,
+    navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
     responsive: {
       0: {
         items: 1,
       },
       600: {
-        items: 3,
+        items: 1,
       },
       1000: {
         items: 1,
