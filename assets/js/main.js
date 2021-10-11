75149ba8453ceaae9b38fc3e47e1acdeb0311f9c
alisharafaldinclear <alisharafaldin@gmail.com>
2021-10-11 12:48:49 +0300

    Change_5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 $(function () {
+
+  // Fixed Header
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 30 ) {
+      $('.header').addClass('fixed-header');
+    } else {
+      $('.header').removeClass('fixed-header');
+    }
+  });
+
+  // Show & Close menu
   $(".show-menu").click(function () {
     $(".header .menu-website").addClass("show");
   });
@@ -9,6 +20,26 @@ $(function () {
 
   // تشغيل السلايدر
   $(".slider .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+          },
+    },
+  });
+
+  // Trigger Testimonial
+
+  $(".testimonial .owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
